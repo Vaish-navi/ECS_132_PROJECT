15520f8466b734ebe622f1638491bf73c15987a4
Pouneh Aghababazadeh <paghababazadeh@gmail.com>
2016-11-07 16:31:17 -0800

    Adding information for Q3.2 to Q3.4

--- Design.R
+++ Design.R
@@ -23,15 +23,77 @@ binaryMessage = StringToBinary(secretMessage, messageLen)
 lenBinaryMessage = as.numeric(length(binaryMessage))
 covertPackets = NULL
 timeStream = 0
+covertDataDelay = NULL
 
 for (i in 1:lenBinaryMessage) {
   if(binaryMessage[i] == 0){
     timeStream <- timeStream + 0.25
+    covertDataDelay <- c(covertDataDelay,0.25)
   }#if bit is 0, delay is 0.25
   else{
     timeStream <- timeStream + 0.75
+    covertDataDelay <- c(covertDataDelay,0.75)
   }#if bit is 1, delay is 0.75
   covertPackets <- c(covertPackets,timeStream)
+  
 }#loop to generate packet stream from binary message  
-covertPackets
+
+#Q2. Generating the Histogram
+temp = 0 #holds the previous timestamp
+overtDataDelay = numeric(dim(overtData)[1])
+
+for( i in 1:dim(overtData)[1])
+{
+  overtDataDelay[i] = overtData[i,2] - temp
+  temp = overtData[i,2]
+}
+
+hist(overtDataDelay)
+hist(covertDataDelay)
+
+
+#Eve will be suspicious of the 0.75 & 0.25 because it's exactly two unique differences, whereas for the "Overt" data, given to us in the CSV, the histogram looks more like a negative exponential distribution, which is what you would expect of this type of time dependent data
+
+
+
+#Q3
+set.seed(1237)
+m = median(overtDataDelay)
+min = min(overtDataDelay)
+max = max(overtDataDelay)
+
+semiRandomPacketsDelay = NULL
+semiRandomPackets = NULL
+timeStream = 0
+
+for (i in 1:lenBinaryMessage) {
+  if(binaryMessage[i] == 0){
+    timeLapse = runif(1, min, m)
+  }#if bit is 0, delay is 0.25
+  else{
+    timeLapse = runif(1, m, max)
+  }#if bit is 1, delay is 0.75
+      timeStream <- timeStream + timeLapse
+      semiRandomPacketsDelay <- c(semiRandomPacketsDelay,timeLapse)
+  semiRandomPackets <- c(semiRandomPackets,timeStream)
+}#loop to generate packet stream from binary message  
+
+
+
+#Q4
+hist(semiRandomPacketsDelay)
+hist(overtDataDelay)
+
+
+
+
+
+
+
+
+
+
+
+
+
 
