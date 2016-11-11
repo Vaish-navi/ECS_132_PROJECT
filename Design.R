82907412e8a83cf4918aed483df7391fd726bf3b
Vaish-navi <vaishnavi.kcv@gmail.com>
2016-11-11 15:44:45 -0800

    Part 5

--- Design.R
+++ Design.R
@@ -1,4 +1,4 @@
-overtData <- read.csv(file = "/Users/pouneh/Desktop/ECS_132_PROJECTB/Traffic_data_orig.csv", header = TRUE,sep = ",")
+overtData <- read.csv(file = "Traffic_data_orig.csv", header = TRUE,sep = ",")
 secretMessage <- "this is a secret message"
 messageLen = as.numeric(nchar(secretMessage))
 
@@ -88,23 +88,88 @@ hist(overtDataDelay)
 #Q5
 set.seed(1237)
 #m = median(overtDataDelay)
-m = quantile(overtDataDelay, probs = c(0.75) ,names= FALSE)
+#m = quantile(overtDataDelay, probs = c(0.75) ,names= FALSE)
+m - mean(overtDataDelay)
 min = min(overtDataDelay)
 max = max(overtDataDelay)
 
-semiRandomPacketsDelay = NULL
-semiRandomPackets = NULL
+semiRandomPacketsDelayB = NULL
+semiRandomPacketsB = NULL
 timeStream = 0
+Nexp <- rexp(lenBinaryMessage, m)
 
 for (i in 1:lenBinaryMessage) {
   if(binaryMessage[i] == 0){
-    timeLapse = runif(1, min, m)
+    timelapse = Nexp[i] 
+    #timeLapse = rexp(1, m)
   }#if bit is 0, delay is 0.25
   else{
-    timeLapse = runif(1, m, max)
+    timeLapse = Nexp[i] + (i^2 * 0.00025)
+    #timeLapse = runif(1, m)
   }#if bit is 1, delay is 0.75
       timeStream <- timeStream + timeLapse
-      semiRandomPacketsDelay <- c(semiRandomPacketsDelay,timeLapse)
-  semiRandomPackets <- c(semiRandomPackets,timeStream)
+      semiRandomPacketsDelayB <- c(semiRandomPacketsDelayB,timeLapse)
+  semiRandomPacketsB <- c(semiRandomPacketsB,timeStream)
 }#loop to generate packet stream from binary message  
-hist(semiRandomPacketsDelay)
+hist(semiRandomPacketsDelayB)
+
+###################### PART #2 DETECTION ##########################
+set.seed(1237)
+
+#Step 1
+x <- rnorm(30,mean = 0,sd = 1)
+y <- rnorm(30,mean = 0,sd = 1)
+qqplot(x,y, plot.it = TRUE)
+
+
+#Step 2
+x <- rnorm(100,mean = 0,sd = 1)
+y <- rnorm(100,mean = 0,sd = 1)
+qqplot(x,y, plot.it = TRUE)
+
+x <- rnorm(1000,mean = 0,sd = 1)
+y <- rnorm(1000,mean = 0,sd = 1)
+qqplot(x,y, plot.it = TRUE)
+
+#It is clear that the shape of the qqplot becomes more linear as n increases.
+#This makes sense as the two distributions are exactly the same.
+#The points are dense on the graph between the 1st and 3rd quartiles; they are especially dense around the mean. 
+
+#Stem 3
+x <- rnorm(100,mean = 0,sd = 1)
+y <- rnorm(100,mean = 5,sd = 3)
+qqplot(x,y, plot.it = TRUE)
+#The shape of this qqplot also appears to be linear despite the fact that the means and standard deviations are different.
+#Obviously, this shows us that the qqplot only indicates the similarity of two distributions regardless of their individual parameters. 
+
+#Step 4
+x <- rexp(100,rate = 1)
+y <- rexp(100,rate = 1)
+qqplot(x,y, plot.it = TRUE)
+
+x <- rexp(1000,rate = 1)
+y <- rexp(1000,rate = 1)
+qqplot(x,y, plot.it = TRUE)
+
+#It is clear that the shape of the qqplot becomes more linear as n increases.
+#This makes sense as the two distributions are exactly the same.
+#The points are dense around 1 which makes sense as the mean for an exponencial distribution is (lamda)^-1. 
+
+#Step 5
+x <- rnorm(100,mean = 0,sd = 1)
+y <- rexp(100,rate = 1)
+qqplot(x,y, plot.it = TRUE)
+
+x <- rnorm(500,mean = 0,sd = 1)
+y <- rexp(500,rate = 1)
+qqplot(x,y, plot.it = TRUE)
+
+#Step 6
+qqplot(overtDataDelay, covertDataDelay, plot.it = TRUE)
+
+#Step 7
+qqplot(overtDataDelay, semiRandomPacketsDelay, plot.it = TRUE)
+
+#Step 8
+qqplot(overtDataDelay, semiRandomPacketsDelayB, plot.it = TRUE)
+
